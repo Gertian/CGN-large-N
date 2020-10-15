c7aa3e13bbc16da00a19f5517ef6f9a06e2a6aa4
Daan Maertens <daan.maertens@gmail.com>
2020-10-15 20:11:20 +0200

    added massive GN and tdvp

--- main.jl
+++ main.jl
@@ -7,103 +7,159 @@ using LsqFit
 using LaTeXStrings
 using LinearAlgebra
 
-function sc_ek(s, p, x, y, k)
-    return sqrt(y^2*p^2 + x^2*s^2*cos(k/2)^2 + 4*sin(k/2)^2 )
+######################
+#Note!:
+#       x is coupling of sigma
+#       y is coupling of pi
+#       m is mass (perturbation)
+#Note Note:
+#   Daan defines x as g^2 <- independent of N
+######################
+
+function sc_ek(s, p, x, y, m, k)
+    return sqrt(y^2*p^2+(x*s-m)^2*cos(k/2)^2 + 4*sin(k/2)^2 )
 end
-function sc_energy(s, p, x, y)
-    a =  (x*s^2+y*p^2)/2 - quadgk( k -> 1/(2*pi)*sc_ek(s,p,x,y,k)   , -pi, pi, rtol=1e-12)[1]
+function sc_energy(s, p, x, y, m)
+    a =  (x*s^2+y*p^2)/2 - quadgk( k -> 1/(2*pi)*sc_ek(s, p, x, y, m, k)   , -pi, pi, rtol=1e-12)[1]
     return a
 end
 
-function sc_dsigma(s, p, x, y)
-    return x*s - quadgk(k -> (x^2*s*cos(k/2)^2)/(2*pi*sc_ek(s, p, x, y, k)), -pi, pi, rtol=1e-12)[1]
+function sc_dsigma(s, p, x, y, m)
+    return x*s - (x*s-m)*x*quadgk(k -> (cos(k/2)^2)/(2*pi*sc_ek(s, p, x, y, m, k)), -pi, pi, rtol=1e-12)[1]
 end
-function sc_dpi(s, p, x, y)
-    return y*p - quadgk(k -> (y^2*p)/(2*pi*sc_ek(s, p, x, y, k)), -pi, pi, rtol=1e-12)[1]
+function sc_dpi(s, p, x, y, m)
+    return y*p - quadgk(k -> (y^2*p)/(2*pi*sc_ek(s, p, x, y, m, k)), -pi, pi, rtol=1e-12)[1]
 end
-function sc_ps(s, p, x, y)
+function sc_ss(s, p, x, y, m)
     out = 0.
     out += x
-    out += quadgk(k -> (x^4*s^2*cos(k/2)^4)/(2*pi*sc_ek(s, p, x, y, k)^3), -pi, pi, rtol=1e-12)[1]
-    out -= quadgk(k -> (x^2    *cos(k/2)^2)/(2*pi*sc_ek(s, p, x, y, k)  ), -pi, pi, rtol=1e-12)[1]
+    out += quadgk(k -> (x^2*(x*s-m)^2*cos(k/2)^4)/(2*pi*sc_ek(s, p, x, y, m, k)^3), -pi, pi, rtol=1e-12)[1]
+    out -= quadgk(k -> (x^2    *cos(k/2)^2)/(2*pi*sc_ek(s, p, x, y, m, k)  ), -pi, pi, rtol=1e-12)[1]
     return out
 end
-function sc_pp(s, p, x, y)
+function sc_pp(s, p, x, y, m)
     out = 0.
     out += y
-    out += quadgk(k -> (y^4*p^2)/(2*pi*sc_ek(s, p, x, y, k)^3), -pi, pi, rtol=1e-12)[1]
-    out -= quadgk(k -> (y^2    )/(2*pi*sc_ek(s, p, x, y, k)  ), -pi, pi, rtol=1e-12)[1]
+    out += quadgk(k -> (y^4*p^2)/(2*pi*sc_ek(s, p, x, y, m, k)^3), -pi, pi, rtol=1e-12)[1]
+    out -= quadgk(k -> (y^2    )/(2*pi*sc_ek(s, p, x, y, m, k)  ), -pi, pi, rtol=1e-12)[1]
     return out
 end
-function sc_pm(s, p, x, y)
+function sc_ps(s, p, x, y, m)
     out = 0.
-    out += quadgk(k -> (x^2*y^2*s*p*cos(k/2)^2)/(2*pi*sc_ek(s, p, x, y, k)^3), -pi, pi, rtol=1e-12)[1]
+    out += quadgk(k -> (x*(x*s-m)*y^2*p*cos(k/2)^2)/(2*pi*sc_ek(s, p, x, y, m, k)^3), -pi, pi, rtol=1e-12)[1]
 end
-function calc_order_parameters(x, y)
-    fun(order) = sc_energy(order[1], order[2], x, y)
+function calc_order_parameters(x, y, m = 0)
+    fun(order) = sc_energy(order[1], order[2], x, y, m)
     function der!(G, order)
-        G[1] = sc_dsigma(order[1], order[2], x, y)
-        G[2] =    sc_dpi(order[1], order[2], x, y)
+        G[1] = sc_dsigma(order[1], order[2], x, y, m)
+        G[2] =    sc_dpi(order[1], order[2], x, y, m)
     end
     function   h!(H, order)
-        H[1,1] = sc_ps(order[1], order[2], x, y)
-        H[1,2] = sc_pm(order[1], order[2], x, y)
+        H[1,1] = sc_ss(order[1], order[2], x, y, m)
+        H[1,2] = sc_ps(order[1], order[2], x, y, m)
         H[2,1] = H[1,2]
-        H[2,2] = sc_pp(order[1], order[2], x, y)
+        H[2,2] = sc_pp(order[1], order[2], x, y, m)
     end
     if x == 0 && y == 0
-        return (0., 0., 0.)
-    else
-        (a,b) = abs.(optimize(fun, der!, h!,  [1., 1.]).minimizer)
-        if x == 0
-            a = 0
-        elseif y == 0
-            b = 0
+        if m == 0
+            return (0., 0., 0., 0., 0.)
+        else
+            return -m * quadgk(k -> cos(k/2)^2 / (2*pi*sc_ek(s,p,0,0,m,k)), -pi, pi, rtol=1e-12)[1]
         end
-        return (a,b, atan(b/a))
+    else
+        if m > 0
+            lower = [0.]
+            upper = [Inf]
+            initial = [-1.,1.]
+        else
+            lower = [-Inf]
+            upper = [0.]
+            initial = [1.,1.]
+        end
+        a,b = optimize(fun,der!,h!,initial).minimizer
+        if m == 0
+            a = abs(a)
+            b = abs(b)
+            if x == 0
+                a = 0
+            elseif y == 0
+                b = 0
+            end
+            return (a,b, atan(b/a),0.)
+        else
+            false_a = optimize(fun,der!,h!,lower,upper,-1 .*initial).minimizer[1]
+        end
+        return (a,b, atan(b/a),false_a)
     end
 end
-function calc_energy(x, y)
-    sigma, pi, _ = calc_order_parameters(x,y)
-    return sc_energy(sigma, pi, x, y)
+function calc_energy(x, y, m)
+    sigma,pi,_,sigma_false = calc_order_parameters(x, y, m)
+    return sc_energy(sigma, pi, x, y, m),sc_energy(sigma_false, pi, x, y, m)
 end
 function calc_lamhes(x ,y)
-    sigma, pi, _ = calc_order_parameters(x,y)
-    ps = sc_ps(sigma, pi, x, y)
-    pp = sc_pp(sigma, pi, x, y)
-    pm = sc_pm(sigma, pi, x, y)
-    return (pp + ps)/2 - sqrt(pm^2 + (pp - ps)^2/4), (pp + ps)/2 + sqrt(pm^2 + (pp - ps)^2/4)
+    sigma, pi, _ , _= calc_order_parameters(x,y)
+    ss = sc_ss(sigma, pi, x, y, m)
+    pp = sc_pp(sigma, pi, x, y, m)
+    ps = sc_ps(sigma, pi, x, y, m)
+    return (pp + ss)/2 - sqrt(ps^2 + (pp - ss)^2/4), (pp + ss)/2 + sqrt(ps^2 + (pp - ss)^2/4)
 end
 
-##########3
-#make some plots specific for my CGN project
-##########
+function plot_cut(x, ys, ms)
+    if length(ys) == 1
+        orders   = map(mi -> calc_order_parameters(x, ys, mi), ms )
+        energies = map(mi -> calc_energy(x, ys, mi), ms)
+        energies_true = map(Ei -> Ei[1], energies)
+        energies_false = map(Ei -> Ei[2], energies)
+        sigmas = map(xi -> xi[1], orders)
+        sigmas_false = map(xi -> xi[4], orders)
 
-function plot_cut(gx, gys)
-    orders   = map(gyi -> calc_order_parameters(gx, gyi)    , gys )
-    energies = map(gyi ->           calc_energy(gx, gyi)    , gys )
-    hess     = map(gyi ->           calc_lamhes(gx, gyi)[1], gys )
-    sigmas = map(xi -> xi[1], orders)
-    pis    = map(xi -> xi[2], orders)
+        p1 = scatter(ms, sigmas, title = "at gx = $(x)", xlabel = L"ma", label = L"<\sigma>")
+        scatter!(p1, ms, sigmas_false                                 , label = L"<\sigma_f>")
 
-    p1 = scatter(gys, sigmas, title = "at gx = $(gx)", xlabel = L"gy", label = L"<\sigma>")
-    scatter!(p1, gys, pis                                            , label = L"<\pi>")
+        p2 = scatter(ms, energies_true, title = "at gx = $(x)", xlabel = L"ma", label = L"<H>")
 
-    p2 = scatter(gys, energies, title = "at gx = $(gx)", xlabel = L"gy", label = L"<H>")
+        p3 = scatter(sigmas, energies_true   , title = "at gx = $(x)", xlabel = L"<\sigma>", label = L"<H>")
+        scatter!(p3, sigmas, energies_false                                               , label = L"<H_f>")
 
-    p3 = scatter(gys, log.(hess)    , title = "at gx = $(gx)", xlabel = L"gy", label = L"min(\lambda_{H})")
+        E0s = map(sigma -> sc_energy(sigma,0., x, 0., 0.), sigmas)
+        test_sigmas = range(-0.3, stop = 0.3, length = 100)
+        chosen_m = 0.005
+        #scatter!(p3, test_sigmas, map(test_sigma -> sc_energy(test_sigma,0.,x,0.,m), label = L"Theory")
+        p4 = scatter(sigmas,E0s .+ chosen_m .* sigmas, title = "at g = $(1.3), m = $(chosen_m)", xlabel = L"<\sigma>", label = L"E_{MF,t}")
+        scatter!(p4, test_sigmas,map(test_sigma -> sc_energy(test_sigma, 0, x, 0, chosen_m), test_sigmas), label = L"E_{calc}")
+        scatter!(p4, sigmas_false,E0s .+ chosen_m .* sigmas_false, label = L"E_{MF,f}")
+        return p1,p2,p3,p4
 
-    return p1, p2, p3
+    elseif length(ms) == 1
+        orders   = map(yi -> calc_order_parameters(x, yi, m)    , ys )
+        energies = map(yi ->           calc_energy(x, yi, m)[1]    , ys )
+        hess     = map(yi ->           calc_lamhes(x, yi, m)[1], ys )
+        sigmas = map(xi -> xi[1], orders)
+        pis    = map(xi -> xi[2], orders)
+
+        p1 = scatter(gys, sigmas, title = "at gx = $(x)", xlabel = L"gy", label = L"<\sigma>")
+        scatter!(p1, gys, pis                                            , label = L"<\pi>")
+
+        p2 = scatter(gys, energies, title = "at gx = $(x)", xlabel = L"gy", label = L"<H>")
+
+        p3 = scatter(gys, log.(hess)    , title = "at gx = $(x)", xlabel = L"gy", label = L"min(\lambda_{H})")
+
+        return p1, p2, p3
+
+    else
+        println("Not yet implemented")
+        # need 3D plot
+    end
+
 end
 
-p1, p2, p3 = plot_cut(2., range(1.2, stop = 1.3, length = 100))
-out = plot(p1,p2,p3, size = (2000, 1000))
-savefig(out, "cut_MF")
+#p1,p2,p3,p4 = plot_cut(1.3, 0,range(-0.1, stop = 0.1, length = 100))
+#out = plot(p1,p2,p3,p4, size = (2000, 1000))
+#savefig(out, "test_cut_MF.pdf")
 
-##################################################################
-##################################################################
-##################################################################
-
+####################################
+####################################
+####################################
 mutable struct state
     length::Int
 
@@ -112,6 +168,7 @@ mutable struct state
 
     sigma::Array{Float64}
        pii::Array{Float64}
+       mis::Array{Float64}
 end
 function sigma(state, i,j,k)
     return state.sigma[mod1(i, state.length)]
@@ -139,7 +196,7 @@ function pii(state, i,j,k)
     return 0.5*(pii(state, i,j)-pii(state, j,k))
 end
 
-function init_state(length, sigma_0, pi_0; BC = "triv")
+function init_state(length, sigma_0, pi_0; BC = "triv", mis = Float64[])
     sis = Float64[]
     pis = Float64[]
     if BC == "triv"
@@ -193,7 +250,7 @@ function init_state(length, sigma_0, pi_0; BC = "triv")
         push!(pi_ops, tmp)
     end
 
-    return state(length, si_ops, pi_ops, sis, pis)
+    return state(length, si_ops, pi_ops, sis, pis, mis)
 end
 function eval_fluctuations(state, gx, gy)
     L = state.length
@@ -221,8 +278,8 @@ function make_H(state, gx, gy)
     out = ComplexF64.(zeros(L,L))
     for i in 1:L
         out[mod1(i,L)  ,mod1(i,L)]   +=      gy*pii(state, i-1,i,i+1)
-        out[mod1(i+1,L),mod1(i  ,L)] +=      im+0.5*im*gx*sigma(state, i-1,i,i+1,i+2)
-        out[mod1(i  ,L),mod1(i+1,L)] += conj(im+0.5*im*gx*sigma(state, i-1,i,i+1,i+2)  )
+        out[mod1(i+1,L),mod1(i  ,L)] +=      im+0.5*im*(gx*sigma(state, i-1,i,i+1,i+2)+state.mis[i]*(-1)^i)
+        out[mod1(i  ,L),mod1(i+1,L)] += conj(im+0.5*im*(gx*sigma(state, i-1,i,i+1,i+2) +state.mis[i]*(-1)^i) )
     end
     return out
 end
@@ -234,27 +291,57 @@ function update_state(state, gx, gy)
     eigvals = eigs.values
     v       = eigs.vectors
     indmax = findlast(xi -> xi<=0., eigvals)
-    v = v'
+    #@show eigvals[1:3]
+    #v = v'
     #at this point we have that eigvecs'*diagm(eigvals)*eigvecs = H
     for i in 1:L
-        tmp = real(sum(diag(v*sigma_op(state, i,i+1, i+2)*v')[1:indmax]))
+        tmp = real(sum(diag(v'*sigma_op(state, i,i+1, i+2)*v)[1:indmax]))
         sigma!(state, i, i+1, i+2, tmp)
 
-        tmp = real(sum(diag(v*pii_op(state, i,i+1)*v')[1:indmax]))
+        tmp = real(sum(diag(v'*pii_op(state, i,i+1)*v)[1:indmax]))
         pii!(state, i, i+1, tmp)
     end
     energy = sum(eigvals[1:indmax]) + gx/4*norm(state.sigma)^2 + gy/4*norm(state.pii)^2
 
     return state, energy
 end
-function plot_state(gx, gy, state; BC = "triv")
-    s0 , p0, _ = calc_order_parameters(gx, gy)
+function evolve_state(dt, state, v, gx, gy, tol)
+    L = state.length
+
+    H = make_H(state, gx, gy)
+    eigs = eigen(H)
+    eigvals = eigs.values
+    expiHt = exp(-im*dt*H)
+    #expiHt = exp(-im*dt*make_H(state, gx, gy))
+    indmax = findlast(xi -> xi<=0., eigvals)
+    #evolved_v = v*diagm(1.0 .-im*dt.*eigvals)
+    evolved_v = expiHt*v
+    #@show maximum(real(expiHt)),maximum(imag(expiHt))
+    #@show diag(expiHt-I)[1:20]
+    #@show maximum(real.(evolved_v.-v)),maximum(imag.(evolved_v.-v))
+    for i in 1:L
+        tmp = real(sum(diag(evolved_v'*sigma_op(state, i,i+1, i+2)*evolved_v)[1:indmax]))
+        sigma!(state, i, i+1, i+2, tmp)
+
+        tmp = real(sum(diag(evolved_v'*pii_op(state, i,i+1)*evolved_v)[1:indmax]))
+        pii!(state, i, i+1, tmp)
+    end
+    energy = sum(eigvals[1:indmax]) + gx/4*norm(state.sigma)^2 + gy/4*norm(state.pii)^2
+
+    return state, energy, evolved_v, eigvals
+end
+function plot_state(gx, gy, m, state; BC = "triv")
+    s0 , p0, _ , s0_false= calc_order_parameters(gx, gy, m)
+    s0_negm , p0_negm, _ , s0_false_negm= calc_order_parameters(gx, gy, -m)
     iis = Int.([range(0, stop=state.length, length=state.length+1)...])
     if BC == "triv"
         a = plot(title = "order parameters")
-        hline!([s0], label = "vaccum expetance simga", color = "blue", linestyle = :dot)
-        hline!([p0], label = "vaccum expetance  pi", color = "orange", linestyle = :dot)
-
+        hline!([s0], label = "vaccum expectance sigma", color = "blue", linestyle = :dot)
+        hline!([s0_negm], label = "s0_negm", color = "red", linestyle = :dot)
+        hline!([s0_false],label = "s0_false", color = "green", linestyle = :dot)
+        if gy != 0
+            hline!([p0], label = "vaccum expectance  pi", color = "orange", linestyle = :dot)
+        end
         scatter!(iis.+0.5, map(ii-> (-1)^ii*pii(state, ii, ii+1),iis), color = "orange", label = L"pi_{i, i+1}" )
         scatter!(iis    , map(ii-> (-1)^(ii-1)*sigma(state, ii-1, ii, ii+1),iis), color = "blue"  , label = L"\sigma_{i-1, i, i+1}")
 
@@ -262,8 +349,8 @@ function plot_state(gx, gy, state; BC = "triv")
         return plot(a, size = (2000, 1000))
     elseif BC == "fluc_triv"
         a = plot(title = "fluctuations of the order parameters")
-        hline!([s0], label = "vaccum expetance simga", color = "blue", linestyle = :dot)
-        hline!([p0], label = "vaccum expetance  pi", color = "orange", linestyle = :dot)
+        hline!([s0], label = "vaccum expectance simga", color = "blue", linestyle = :dot)
+        hline!([p0], label = "vaccum expectance  pi", color = "orange", linestyle = :dot)
 
         fs, fp = eval_fluctuations(state, gx, gy)
 
@@ -274,8 +361,8 @@ function plot_state(gx, gy, state; BC = "triv")
         return plot(a, size = (2000, 1000))
     elseif BC == "pi_kink"
         a = plot(title = "order parameters")
-        hline!([s0], label = "vaccum expetance simga", color = "blue", linestyle = :dot)
-        hline!([p0, -p0], label = "vaccum expetance  pi", color = "orange", linestyle = :dot)
+        hline!([s0], label = "vaccum expectance simga", color = "blue", linestyle = :dot)
+        hline!([p0, -p0], label = "vaccum expectance  pi", color = "orange", linestyle = :dot)
         vline!([0.5, state.length/2+0.5, state.length+0.5], color = "red", label = "kink locations")
 
         plot!(iis.+0.5, map(ii-> (-1)^ii*pii(state, ii, ii+1),iis), color = "orange", label = L"pi_{i, i+1}", marker = :dot )
@@ -284,8 +371,8 @@ function plot_state(gx, gy, state; BC = "triv")
         return plot(a, size = (2000, 1000))
     elseif BC == "sigma_kink"
         a = plot(title = "order parameters")
-        hline!([s0, -s0], label = "vaccum expetance simga", color = "blue", linestyle = :dot)
-        hline!([p0], label = "vaccum expetance  pi", color = "orange", linestyle = :dot)
+        hline!([s0, -s0], label = "vaccum expectance simga", color = "blue", linestyle = :dot)
+        hline!([p0], label = "vaccum expectance  pi", color = "orange", linestyle = :dot)
         vline!([0., state.length/2, state.length], color = "red", label = "kink locations")
 
         plot!(iis.+0.5, map(ii-> (-1)^ii*pii(state, ii, ii+1),iis), color = "orange", label = L"pi_{i, i+1}", marker = :dot )
@@ -347,10 +434,102 @@ function make_datapoint(gx, gy, L; tol = 1e-2)
     return plot_triv, plot_piki, plot_siki, plot_fluc, tene, pene, sene
 end
 
+function potential_trapping(gx, L, mo, trial_mo; tol = 1e-2)
 
+    function make_mis(L,mo)
+        ms = []
+        for i in 1:Int(L/2-20)
+            push!(ms,-mo)
+        end
+        for i in Int(L/2-19):Int(L/2+19)
+            push!(ms,mo)
+        end
+        for i in Int(L/2+20):L
+            push!(ms,-mo)
+        end
+        return ms
+    end
 
+    @assert iseven(L)
+    gy = 0.
+    s0, p0, _ = calc_order_parameters(gx, gy, trial_mo)
+    e0 = L/2*calc_energy(gx, gy, trial_mo)[1]
+    #potential trapping for initial state
+    triv = init_state(L, s0, p0; BC = "triv", mis = make_mis(L,trial_mo))
 
-ref,piking,sigmakink,flucplot,_=make_datapoint(4.5, 1.5, 100; tol = 1e-12)
-savefig(plot(ref, piking, sigmakink, flucplot), "MF_kink")
+    terr = Inf
+    tene = Inf
+    while terr > tol
+        triv, tene_n = update_state(triv, gx, gy)
+        terr = abs(tene-tene_n)
+        tene = tene_n
+        @show tene, terr
+        plot_triv = plot_state(gx, gy, trial_mo, triv; BC = "triv")
+        display(plot_triv)
+    end
+    s0, p0, _ = calc_order_parameters(gx, gy, mo)
+    triv.mis = make_mis(L,mo)
+    plot_triv = 0
 
+    terr = Inf
+    tene = Inf
+    while terr > tol
+        triv, tene_n = update_state(triv, gx, gy)
+        terr = abs(tene-tene_n)
+        tene = tene_n
+        @show tene, terr
+        plot_triv = plot_state(gx, gy, mo, triv; BC = "triv")
+        display(plot_triv)
+    end
+    triv.mis = [range(mo;stop=mo,length=L)...]
+    #return plot_triv,triv
 
+    terr = Inf
+    tene = Inf
+    while terr > tol
+        triv, tene_n = update_state(triv, gx, gy)
+        terr = abs(tene-tene_n)
+        tene = tene_n
+        @show tene, terr
+        plot_triv = plot_state(gx, gy, mo, triv; BC = "triv")
+        display(plot_triv)
+    end
+    plot_triv = plot_state(gx, gy, mo, triv; BC = "triv")
+    plot(plot_triv, title = "m = $(mo), t = 0")
+    return plot_triv,triv
+end
+
+function time_evolution(state, dt, N, gx, mo; tol = 1e-2)
+    gy = 0.
+    H = make_H(state, gx, gy)
+    eigs = eigen(H)
+    v       = eigs.vectors
+    prev_eigvals = eigs.values
+    state_plot = plot_state(gx, gy, mo, state; BC = "triv")
+    display(state_plot)
+    for step in 1:N
+        state, energy, v, eigvals = evolve_state(dt, state, v, gx, gy, tol)
+        @show maximum(prev_eigvals.-eigvals)
+        prev_eigvals = eigvals
+        state_plot = plot_state(gx, gy, mo, state; BC = "triv")
+        display(state_plot)
+        @show (step,energy)
+    end
+    state_plot = plot_state(gx, gy, mo, state; BC = "triv")
+    plot(state_plot, title = "t=$(N*dt), dt=$(dt)")
+    return state_plot,state
+end
+
+#ref,piking,sigmakink,flucplot,_=make_datapoint(4.5, 1.5, 100; tol = 1e-12)
+#savefig(plot(ref, piking, sigmakink, flucplot), "MF_kink")
+#initial_state = potential_trapping(1.3^2,100, 0.05, 1.; tol = 1e-4)
+dts = [0.01,0.005,0.001]
+total_time = 0.1
+plot_initial,initial_state = potential_trapping(1.3^2,150, 0.005, 1.; tol = 1e-5);
+for dt in dts
+    N = Int(round(total_time/dt))
+    plot_final,final_state = time_evolution(deepcopy(initial_state),dt,N,1.3^2,0.005; tol = 1e-05);
+    #final_state = deepcopy(initial_state)
+    #plot_final,final_state = time_evolution(final_state,initial_state,dt,N,1.3^2,0.005);
+    savefig(plot(plot_initial,plot_final), "Test_evolution_$(dt).png")
+end
